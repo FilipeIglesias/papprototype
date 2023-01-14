4eed70e121f206ff64cfe4689721a0444fa44cab
FilipeIglesias <98523315+FilipeIglesias@users.noreply.github.com>
2023-01-14 14:08:12 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'TimeZone',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSwatch().copyWith(
           primary: const Color(0xFF0A2647),
