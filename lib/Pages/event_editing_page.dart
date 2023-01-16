367b21f5943f9a43b0f12a9c498e965122e96da9
FilipeIglesias <98523315+FilipeIglesias@users.noreply.github.com>
2023-01-16 15:27:27 +0000

    Seleção de Datas

--- lib/Pages/event_editing_page.dart
+++ lib/Pages/event_editing_page.dart
@@ -88,33 +88,135 @@ class _EventEditingPageState extends State<EventEditingPage> {
   Widget buildDateTimePickers() => Column(
         children: [
           buildFrom(),
+          buildTo(),
         ],
       );
 
-  Widget buildFrom() => Row(
-        children: [
-          Expanded(
-            flex: 2,
-            child: buildDropdownField(
-              text: Utils.toDate(fromDate),
-              onClicked: () {},
+  Widget buildFrom() => buildHeader(
+        header: 'FROM',
+        child: Row(
+          children: [
+            Expanded(
+              flex: 2,
+              child: buildDropdownField(
+                text: Utils.toDate(fromDate),
+                onClicked: () => pickFromDateTime(pickDate: true),
+              ),
             ),
-          ),
-          Expanded(
-            child: buildDropdownField(
-              text: Utils.toTime(fromDate),
-              onClicked: () {},
+            Expanded(
+              child: buildDropdownField(
+                text: Utils.toTime(fromDate),
+                onClicked: () => pickFromDateTime(pickDate: false),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       );
+
+  Widget buildTo() => buildHeader(
+        header: 'TO',
+        child: Row(
+          children: [
+            Expanded(
+              flex: 2,
+              child: buildDropdownField(
+                text: Utils.toDate(toDate),
+                onClicked: () => pickToDateTime(pickDate: true),
+              ),
+            ),
+            Expanded(
+              child: buildDropdownField(
+                text: Utils.toTime(toDate),
+                onClicked: () => pickToDateTime(pickDate: false),
+              ),
+            ),
+          ],
+        ),
+      );
+
+  Future pickFromDateTime({required bool pickDate}) async {
+    final date = await pickDateTime(fromDate, pickDate: pickDate);
+    if (date == null) return;
+
+    if (date.isAfter(toDate)) {
+      toDate =
+          DateTime(date.year, date.month, date.day, date.hour, date.minute);
+    }
+
+    setState(() => fromDate = date);
+  }
+
+  Future pickToDateTime({required bool pickDate}) async {
+    final date = await pickDateTime(
+      toDate,
+      pickDate: pickDate,
+      firstDate: pickDate ? fromDate : null,
+    );
+    if (date == null) return;
+
+    if (date.isAfter(toDate)) {
+      toDate =
+          DateTime(date.year, date.month, date.day, date.hour, date.minute);
+    }
+
+    setState(() => fromDate = date);
+  }
+
+  Future<DateTime?> pickDateTime(
+    DateTime initialDate, {
+    required bool pickDate,
+    DateTime? firstDate,
+  }) async {
+    if (pickDate) {
+      final date = await showDatePicker(
+        context: context,
+        initialDate: initialDate,
+        firstDate: DateTime(2022, 12, 31, 23, 9, 59),
+        lastDate: DateTime(2101),
+      );
+
+      if (date == null) return null;
+
+      final time =
+          Duration(hours: initialDate.hour, minutes: initialDate.minute);
+
+      return date.add(time);
+    } else {
+      final timeOfDay = await showTimePicker(
+        context: context,
+        initialTime: TimeOfDay.fromDateTime(initialDate),
+      );
+
+      if (timeOfDay == null) return null;
+
+      final date =
+          DateTime(initialDate.year, initialDate.month, initialDate.day);
+      final time = Duration(hours: timeOfDay.hour, minutes: timeOfDay.minute);
+
+      return date.add(time);
+    }
+  }
 
   Widget buildDropdownField(
-        {required String text, required VoidCallback onClicked}) =>
-    ListTile(
-      title: Text(text),
-      trailing: Icon(Icons.arrow_drop_down),
-      onTap: onClicked,
-    );
+          {required String text, required VoidCallback onClicked}) =>
+      ListTile(
+        title: Text(text),
+        trailing: Icon(Icons.arrow_drop_down),
+        onTap: onClicked,
+      );
 
+  Widget buildHeader({
+    required String header,
+    required Widget child,
+  }) =>
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            header,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          child,
+        ],
+      );
 }
